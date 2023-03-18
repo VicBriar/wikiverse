0bc7496e65c664a5cc2021037b9babd59d864efe
Victoria McCrady <spartanginger@gmail.com>
2023-03-17 17:07:21 -0700

    feat(app.js): use loader function to dynamically change page renders

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -5,9 +5,13 @@ import { PagesList } from './PagesList';
 import apiURL from '../api';
 
 export const App = () => {
-
+//STATE!----------------------
 	const [pages, setPages] = useState([]);
-
+	const [view, setView] = useState({
+		page: "home",
+		url: null
+	})
+//-----------------------------
 	async function fetchPages(){
 		try {
 			const response = await fetch(`${apiURL}/wiki`);
@@ -22,11 +26,32 @@ export const App = () => {
 		fetchPages();
 	}, []);
 
+	function loader () {
+		switch(view.page){
+
+			case 'home':
+			console.log("home")
+				return (<>
+				<h2>An interesting 📚</h2>
+				<PagesList pages={pages} />
+				</>);
+			
+			case 'article':
+			console.log("article")
+			return (<>
+
+			</>);
+			
+			case 'post':
+				//stuff
+			break;
+
+		}
+	}
 	return (
 		<main>	
       <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
+	  {loader()}
 		</main>
 	)
 }
